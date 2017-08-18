350911e4fa74f1c41a919b11b36e3483a588596c
sigitamocevice <sigita.z@gmail.com>
2017-08-18 13:03:55 +0300

    first add

---
+++ src/app/server.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+@Injectable()
+export class ServerService  {
+
+    constructor(private http: Http) {}
+
+    storeServers(servers: any[]) {
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('https://theory-ng-http.firebaseio.com/data.json', 
+            servers, 
+            {headers: headers});
+    }
+
+}
